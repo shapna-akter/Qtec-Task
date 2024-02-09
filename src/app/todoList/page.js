467c5d52cb92ae7e5ffe200a85150d9e204ca7fc
shapna28 <shapna088@gmail.com>
2024-02-09 16:33:41 +0600

    done: show the task on UI

--- src/app/todoList/page.js
+++ src/app/todoList/page.js
@@ -30,18 +30,69 @@ const TodoList = () => {
             <div className="flex flex-wrap justify-center items-center mb-4">
                 <input
                     type="text"
-                    className="lg:w-2/5 w-full border border-primary p-2 text-xl mb-2 lg:mb-0"
+                    className="lg:w-2/5 w-full border border-primary p-2 text-xl mb-2 lg:mb-0 lg:rounded-l-lg"
                     placeholder="Add a new task"
                     value={newTask}
                     onChange={(e) => setNewTask(e.target.value)}
                 />
                 <button
-                    className="bg-primary text-white p-2 text-xl"
+                    className="bg-primary lg:rounded-r-lg text-white p-2 text-xl"
                     onClick={addTask}
                 >
                     Add Task
                 </button>
             </div>
+
+            {tasks.length === 0 ? (
+                <div className="flex justify-center items-center font-bold lg:text-3xl p-12 text-red-500">
+                    Task not assigned
+                </div>
+            ) : (
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-3 lg:gap-0 shadow-md">
+                    {tasks
+                        .map((task) => (
+                            <div
+                                key={task?.id}
+                                className="border-2 text-xs"
+                            >
+                                <div className="text-center">
+                                    <div className="text-xs font-medium border-b-2">
+                                        <p>
+                                            {task.id}
+                                        </p>
+                                    </div>
+                                    <div className="card-body px-2">
+                                        <div className="bg-[#3776D4] text-base-100 py-6 px-2 text-center text-2xl">
+                                            <p>Task: {task?.text}</p>
+                                            <p>Task Priority: {task?.priority}</p>
+                                        </div>
+                                        <div className='flex items-center justify-center gap-2'>
+                                            <input
+                                                type="checkbox"
+                                                className='checkbox  checkbox-md checkbox-primary'
+
+                                            />
+
+                                            <button
+                                                className="bg-[#3776D4] text-white px-2 py-1"
+
+                                            >
+                                                Edit
+                                            </button>
+                                            <button
+                                                className="bg-red-500 text-white px-2 py-1"
+
+                                            >
+                                                Delete
+                                            </button>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                </div>
+            )}
         </div>
     );
 };
